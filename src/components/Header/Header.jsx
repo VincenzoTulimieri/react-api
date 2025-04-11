cc1a4d6871f2997629b52904ebb93177d27a5c77
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-11 15:05:11 +0200

    feat: header fatto

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 export default function Header(){
     return(
-        <header>
-            
+        <header className="bg-info mb-4">
+            <div className="container py-4">
+                <h1 className="text-center text-dark">Lista Attori</h1>
+            </div>
         </header>
     )
 }
